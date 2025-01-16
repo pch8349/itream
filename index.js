3a21a2b99bc24308ad2d1d3ade33266c8e236b42
pch8349 <chan449@naver.com>
2025-01-16 15:33:18 +0900

    주석 수정

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const parser = new Parser({
     
     text += `<ul>`;
     
-    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
+    // 최신 10개 글 제목과 링크를 가져온 후 text에 추가
     for (let i = 0; i < 10; i++) {
         const {title, link} = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
